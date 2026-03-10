fe37f69cf3d43ce5646df90c8abc417532b2e3a4
Tom Gao <zimingg@andrew.cmu.edu>
2026-03-10 16:40:57 -0400

    Refactor Pick skill into PushT subclass

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,8 +1,7 @@
 from .shelf_retrieve_v1 import compute_shelf_success
 from .shelf_scene_builder import ShelfSceneBuilder
 from .shelf_retrieve_v1 import ObjectRetrieveFromShelfEnv
-from .pick_skill_env import PickSkillEnv
 from .place_skill_env import PlaceSkillEnv
-from .pusht_obstacles import PushTWithObstaclesEnv, MoveGoalWithObstaclesEnv, PushCubeWithObstaclesEnv
+from .pusht_obstacles import PushTWithObstaclesEnv, MoveGoalWithObstaclesEnv, PushCubeWithObstaclesEnv, PickSkillEnv
 
 __all__ = ["compute_shelf_success", "ShelfSceneBuilder", "ObjectRetrieveFromShelfEnv", "PushTWithObstaclesEnv", "MoveGoalWithObstaclesEnv", "PushCubeWithObstaclesEnv", "PickSkillEnv", "PlaceSkillEnv"]
